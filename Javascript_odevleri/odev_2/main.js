cc6d65bae3c79e59e31fd2c13438fe8a7cc52d28
Gokhan Cankaya <cankayagok@gmail.com>
2022-05-29 18:22:27 +0300

    javascipt odev 2 guncelleme

--- Javascript_odevleri/odev_2/main.js
+++ Javascript_odevleri/odev_2/main.js
@@ -4,9 +4,32 @@ const taskDom = document.querySelector("#task")
 
 
 function newElement () {
+  
+    const DIVDOM = document.createElement("div")
+    DIVDOM.classList.add("list-item")
+    listDOM.append(DIVDOM)
 
-    const liDOM = document.createElement("li");
+    const liDOM = document.createElement("li"); 
+    liDOM.classList.add("todo-item")
     liDOM.innerHTML =taskDom.value;
-    listDOM.append(liDOM);
+    DIVDOM.append(liDOM);
+
+    const Close =document.createElement("span")
+    Close.classList.add("button")
+    Close.textContent="X";
+    DIVDOM.append(Close)
+
+    Close.addEventListener("click",removelisk)
+    liDOM.addEventListener("click", finishToDo)
+    
+
     taskDom.value="";   
+}
+
+function removelisk (){
+    this.parentElement.remove();
+}
+
+function finishToDo(){
+    this.classList.toggle("checked")
 }
