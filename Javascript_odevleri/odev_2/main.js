6892c7d0e45beffd6f0381319352c8e18cf8910b
Gokhan Cankaya <cankayagok@gmail.com>
2022-05-31 05:58:52 +0300

    toast eklendi

--- Javascript_odevleri/odev_2/main.js
+++ Javascript_odevleri/odev_2/main.js
@@ -1,35 +1,41 @@
 const listDOM = document.querySelector("#list")
 const taskDom = document.querySelector("#task")
 
+function newElement() {
+    if (taskDom.value != "") {
+
+        const DIVDOM = document.createElement("div")
+        DIVDOM.classList.add("list-item")
+        listDOM.append(DIVDOM)
+
+        const liDOM = document.createElement("li");
+        liDOM.classList.add("todo-item")
+        liDOM.innerHTML = taskDom.value;
+        DIVDOM.append(liDOM);
+        $('.dolu').toast('show')
+
+        const Close = document.createElement("span")
+        Close.classList.add("button")
+        Close.textContent = "X";
+        DIVDOM.append(Close)
+
+        Close.addEventListener("click", removelisk)
+        liDOM.addEventListener("click", finishToDo)
+        
+
+        taskDom.value = "";
+    } else {
+        $('.bos').toast('show')
+    }
 
 
-function newElement () {
-  
-    const DIVDOM = document.createElement("div")
-    DIVDOM.classList.add("list-item")
-    listDOM.append(DIVDOM)
 
-    const liDOM = document.createElement("li"); 
-    liDOM.classList.add("todo-item")
-    liDOM.innerHTML =taskDom.value;
-    DIVDOM.append(liDOM);
+    }
 
-    const Close =document.createElement("span")
-    Close.classList.add("button")
-    Close.textContent="X";
-    DIVDOM.append(Close)
-
-    Close.addEventListener("click",removelisk)
-    liDOM.addEventListener("click", finishToDo)
-    
-
-    taskDom.value="";   
-}
-
-function removelisk (){
+function removelisk() {
     this.parentElement.remove();
 }
 
-function finishToDo(){
+function finishToDo() {
     this.classList.toggle("checked")
 }
